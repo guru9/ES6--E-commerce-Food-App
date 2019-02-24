07e70893f9d9e59d6e39bfcb306511982b5dd0a5
Guru <gururaj9m@gmail.com>
2019-02-24 17:48:57 +0530

    inc/dec & del cart item and styles has been updated.

--- index.js
+++ index.js
@@ -30,18 +30,18 @@ addToCartButtonsDom.forEach(addToCartButtonDom => {
                     <li class="list-group-item d-flex justify-content-between align-items-center">
                         <span class="product-name">${product.name}</span>
                         <span><img src="${product.image}" alt="${product.name}"></span>
-                        <span class="badge badge-success badge-pill decrease" data-action="decr_quantity">&minus;</span>
+                        <span class="badge badge-success badge-pill decrease badge-danger" data-action="decr_quantity">&minus;</span>
                         <span class="product-qty">${product.quantity}</span>
                         <span class="badge badge-success badge-pill increase" data-action="incr_quantity">&plus;</span>
                         <span class="badge badge-primary badge-pill">${product.price}</span>
-                        <span class="badge badge-danger badge-pill delete">&times;</span>
+                        <span class="badge badge-danger badge-pill delete" data-action="delete_item">&times;</span>
                     </li>
                 </ul>
-                `);
+            `);
 
             cart.push(product);
             addToCartButtonDom.innerText = 'In Cart';
-            addToCartButtonDom.classList.add("button-disable");
+            addToCartButtonDom.disabled = true;
         }
 
         //cart functionality
@@ -49,22 +49,54 @@ addToCartButtonsDom.forEach(addToCartButtonDom => {
         cartItemDom.forEach(itemSelectedCartItemDom => {
 
             if (itemSelectedCartItemDom.querySelector('.product-name').innerText === product.name) {
-                itemSelectedCartItemDom.querySelector('[data-action="decr_quantity"]').addEventListener('click', () => {
-                    cart.forEach(cartItem => {
-                        if (cartItem.name === product.name && cartItem.quantity > 0) {
-                            itemSelectedCartItemDom.querySelector('.product-qty').innerText = --cartItem.quantity;
-                        }
-                    })
-                })
+
+                //inc product quantity
                 itemSelectedCartItemDom.querySelector('[data-action="incr_quantity"]').addEventListener('click', () => {
                     cart.forEach(cartItem => {
                         if (cartItem.name === product.name) {
                             itemSelectedCartItemDom.querySelector('.product-qty').innerText = ++cartItem.quantity;
+                            itemSelectedCartItemDom.querySelector('[data-action="decr_quantity"]').classList.remove('badge-danger')
                         }
                     })
                 })
+
+                //dec product quantity
+                itemSelectedCartItemDom.querySelector('[data-action="decr_quantity"]').addEventListener('click', () => {
+                    cart.forEach(cartItem => {
+                        if (cartItem.name === product.name) {
+                            if (cartItem.quantity > 1) {
+                                itemSelectedCartItemDom.querySelector('.product-qty').innerText = --cartItem.quantity;
+                            }
+                            else {
+                                itemSelectedCartItemDom.classList.add('cart-item-removed');
+                                setTimeout(() => itemSelectedCartItemDom.remove(), 300);
+                                cart = cart.filter(cartItem => cartItem.name != product.name);
+                                addToCartButtonDom.innerText = 'Add to Cart';
+                                addToCartButtonDom.disabled = false;
+                            }
+                            if (cartItem.quantity === 1) {
+                                itemSelectedCartItemDom.querySelector('[data-action="decr_quantity"]').classList.add('badge-danger');
+                            }
+                        }
+                    })
+                })
+
+                //delete product quantity
+                itemSelectedCartItemDom.querySelector('[data-action="delete_item"]').addEventListener('click', () => {
+                    cart.forEach(cartItem => {
+                        if (cartItem.name === product.name) {
+                            itemSelectedCartItemDom.classList.add('cart-item-removed');
+                            setTimeout(() => itemSelectedCartItemDom.remove(), 300);
+                            cart = cart.filter(cartItem => cartItem.name != product.name);
+                            addToCartButtonDom.innerText = 'Add to Cart';
+                            addToCartButtonDom.disabled = false;
+                        }
+                    })
+                })
+
             }
         });
+
     })
 });
 
